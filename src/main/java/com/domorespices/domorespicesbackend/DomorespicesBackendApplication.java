ea6a54e2164925289896e4ee5cb12cf75ee5beff
Kasi Raj <kasiraj888@gmail.com>
2019-06-11 18:08:15 +0530

    Initial setup

---
+++ src/main/java/com/domorespices/domorespicesbackend/DomorespicesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.domorespices.domorespicesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomorespicesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomorespicesBackendApplication.class, args);
+	}
+
+}
